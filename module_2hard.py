a9506302f15addad78c5f265cf54f266acebe28b
Andrey6691 <abw66@yandex.ru>
2024-11-03 20:57:17 +0300

    Update module_2hard.py

--- module_2hard.py
+++ module_2hard.py
@@ -1,9 +1,6 @@
-parol = int(input('Введите число от 3 до 30:  '))
+parol = int(input('Введите число от 3 до 20:  '))
 for i in range(1, 20):
     for j in range(i+1, 20):
-        if i == j:
-            continue
-        else:
-            sum_ = i + j
-            if parol % sum_ == 0:
-                print(i,j, end =' ')
+        sum_ = i + j
+        if parol % sum_ == 0:
+            print(i,j, end =' ')
